a95c30b242fa302b149882d8741db012351cd19e
jeffrey anthony mukkath <jeffreymukkath65@gmail.com>
2025-01-06 18:56:11 +0530

    add examples for java syntax, output handling, and comments

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("HELLO, WORLD!");
+        System.out.println("NEW FIRST LINE");
+        System.out.println("NEW SECOND LINE");
+        System.out.println("NEW THIRD LINE");
+        System.out.print("DEMONSTRATING PRINT WHICH DOESNT USE NEW LINE. ");
+        System.out.print("AN EXAMPLE SHOWN HERE!");
+        System.out.println(
+                "NOW DEMONSTRATING HOW TO PRINT NUMBER. PRINTING NUMBERS DONT NEED TO BE PUT INSIDE DOUBLE QUOTATIONS");
+        System.out.println(7777777);
+        System.out.println("some mathematical calculation examples");
+        System.out.println(3 + 3);
+        System.out.println(3 * 3);
+        // this is a comment line
+        /*
+         * this is a
+         * multi line comment
+         */
+        System.out.println("demonstrating single line comments");
     }
 }
